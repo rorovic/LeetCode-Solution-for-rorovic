44c37b990c5ed749c7ff26d948dc498ecec9b225
Jinyuan Du (杜金源) <dujinyuan@le.com>
2017-07-18 19:27:56 +0800

    add WordCount

---
+++ src/main/java/solution/mr/WordCount.java
@@ -0,0 +1,91 @@
+package main.java.solution.mr;
+
+/**
+ * Created by dujinyuan on 16/3/13.
+ */
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * upload:
+ * scp yoho_code_mr.jar hadoop@182.92.219.132:~/
+ *
+ * hadoop jar yoho_code_mr.jar /test/user_item_purchase.txt /yoho/test/out1/
+ *
+ */
+public class WordCount {
+
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);//初始化输出value
+        private Text word = new Text();//初始化输出key
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            //读取输入的value
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);//输出 < 单词 , 1 >
+            }
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    /**
+     * hdfs://182.92.219.132:8020/test/user_item_purchase.txt
+       hdfs://182.92.219.132:8020/test/out/
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length < 2) {
+            System.err.println("Usage: wordcount <in> [<in>...] <out>");
+            System.exit(2);
+        }
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(TokenizerMapper.class); //mapper
+        job.setCombinerClass(IntSumReducer.class); //combiner
+        job.setReducerClass(IntSumReducer.class); //reducer
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        for (int i = 0; i < otherArgs.length - 1; ++i) {
+            FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
+        }
+        FileOutputFormat.setOutputPath(job,
+                new Path(otherArgs[otherArgs.length - 1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
+
