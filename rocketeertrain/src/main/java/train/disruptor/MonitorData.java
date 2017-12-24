4250ceb2020a5c187f4d0e0ea52711a3504856ca
杜金源 <dujinyuan02@meituan.com>
2017-12-24 10:26:36 +0800

    模拟数据类

--- rocketeertrain/src/main/java/train/disruptor/MonitorData.java
+++ rocketeertrain/src/main/java/train/disruptor/MonitorData.java
@@ -1,4 +1,16 @@
 package train.disruptor;
 
+/**
+ * 模拟数据类
+ */
 public class MonitorData {
+    private long value;
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
 }
